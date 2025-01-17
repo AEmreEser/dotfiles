7fc68e2555f92d5151f12dcfc1b3a973bb8461bf
A. Emre Eser <101353048+AEmreEser@users.noreply.github.com>
2025-01-17 05:50:22 +0300

    use super/win key for music player controls

--- xmonad.hs
+++ xmonad.hs
@@ -11,17 +11,20 @@ main = do
     spawn "xrandr --output HDMI-1 --auto --primary --output eDP-1 --auto --left-of HDMI-1 &" --dual monitor config
     spawn "xrandr --output HDMI-1-1 --auto --primary --output eDP-1-1 --auto --left-of HDMI-1-1 &" --dual monitor config
 
-    let wallpaper_path = "/home/emre/Pictures/SHORPY-52nd-street-1.jpg" -- SHORPY-mount-vernon-colorado.jpg"
-    spawn $ "xloadimage -onroot -at 0,-200 " ++wallpaper_path++ " -at 1920,-200 " ++wallpaper_path++ " &" --set wallpaper, note that the '$' is necessary for string concatenation
+    let wallpaper_path = "/home/emre/Pictures/" --vintage-high-quality-photos/" -- SHORPY-52nd-street-1.jpg" -- SHORPY-mount-vernon-colorado.jpg"
+    let wallpaper = "resized.jpg" -- "janet-delaney-portfolio/langton-folsom-howard-streets-1979.jpg" -- "SHORPY-mount-vernon-colorado.jpg" -- "SHORPY-52nd-street-1.jpg"
+    let adj = "-200" -- no spaces on either end!!
+    spawn $ "xloadimage -onroot -at 0," ++adj++" " ++wallpaper_path++wallpaper++ " -at 1920,"++adj++" " ++wallpaper_path++wallpaper++ " &" --set wallpaper, note that the '$' is necessary for string concatenation
 
     xmonad $ docks def
-        { layoutHook = avoidStruts  $  layoutHook def
+		{ layoutHook = avoidStruts  $  layoutHook def
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
+						}
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
         }  `additionalKeys`
+			-- mod4: win/super key, mod1: alt
             [ ((0, 0x1008FF11), spawn "amixer -q sset -D pulse Master 10%-")
             , ((0, 0x1008FF13), spawn "amixer -q sset -D pulse Master 10%+")
             , ((shiftMask, 0x1008FF11), spawn "amixer -q sset -D pulse Master 5%-")
@@ -31,19 +34,19 @@ main = do
             , ((0, 0x1008FF02), spawn "lux -a 10%")
             , ((shiftMask, 0x1008FF02), spawn "lux -a 5%")
             , ((shiftMask, 0x1008FF03), spawn "lux -s 5%")
-            , ((mod1Mask, xK_p), spawn "playerctl play-pause")
             , ((mod4Mask, xK_n), spawn "nemo")
             , ((mod4Mask, xK_i), spawn "cinnamon-settings")
-            , ((mod4Mask, xK_o), spawn "opera")
-            , ((mod1Mask, xK_3), spawn "gnome-screenshot -a")
-            , ((mod1Mask .|. controlMask, xK_3), spawn "gnome-screenshot")
+            , ((mod4Mask, xK_o), spawn "zen")
+            , ((mod4Mask .|. controlMask, xK_3), spawn "gnome-screenshot -a")
+            -- , ((mod1Mask .|. controlMask, xK_3), spawn "gnome-screenshot")
+            , ((mod4Mask, xK_p), spawn "playerctl play-pause")
             , ((0, 0x1008FF14), spawn "playerctl play-pause")
             , ((0, 0x1008FF16), spawn "playerctl previous")
             , ((0, 0x1008FF17), spawn "playerctl next")
+            , ((mod4Mask, xK_bracketleft), spawn "playerctl previous")
+            , ((mod4Mask, xK_bracketright), spawn "playerctl next")
             , ((controlMask .|. mod4Mask, xK_s), spawn "systemctl suspend")
             , ((controlMask .|. mod4Mask .|. mod1Mask, xK_equal), spawn "shutdown now")
-            , ((mod1Mask, xK_bracketleft), spawn "playerctl previous")
-            , ((mod1Mask, xK_bracketright), spawn "playerctl next")
             , ((mod4Mask .|. shiftMask, xK_t), spawn "setxkbmap tr")
             , ((mod4Mask .|. shiftMask, xK_u), spawn "setxkbmap us")
             ]
